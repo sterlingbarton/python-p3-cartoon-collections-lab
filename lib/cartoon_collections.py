db6c4ed21bc9363c152bf599c6088c5b3932d6ed
Sterling <sterlingbarton81@gmail.com>
2023-05-04 16:48:44 -0600

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,29 @@
-def roll_call_dwarves():
-    pass
+CHEESES = ["camembert", "gouda", "cheddar"]
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def roll_call_dwarves(names):
+    index = 1
+    for name in names:
+        print(f"{index}. {name}")
+        index += 1
 
-def find_the_cheese():
-    pass
+
+def summon_captain_planet(list):
+    new_list = [f"{name.title()}!" for name in list]
+    return new_list
+
+
+def long_planeteer_calls(list):
+    for name in list:
+        if (len(name) > 4):
+            return True
+
+    return False
+
+
+def find_the_cheese(foods):
+    for food in foods:
+        if food in CHEESES:
+            return food
+
+    return None
